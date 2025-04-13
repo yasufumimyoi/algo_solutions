06f27824e9e291f51d5fa8f97c6b13cca864aa66
yasufumimyoi <yasufumi.myoi@gmail.com>
2025-04-13 17:27:04 +0900

    解答

---
+++ solutions/algo_beginner/problem_2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin", "utf-8").trim();
+const lines = input.trim().split("\n");
+const [len, target] = lines[0].split(' ').map(Number)
+const numbers = lines[1].split(' ').map(Number)
+let count = 0
+
+for (let i = 0; i <= len; i++) {
+  if(numbers[i] === target) {
+    count = count + 1
+  }
+}
+
+console.log(count)
